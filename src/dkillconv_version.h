75c241fdfe3ca048358588f16ac8b4cc6e05cf3c
mefistotelis <mefistotelis@gmail.com>
2015-01-29 23:10:42 +0000

    Started work.
    Started work on Dungeon Keeper Independent Level Layout converter.

---
+++ src/dkillconv_version.h
@@ -0,0 +1,44 @@
+/******************************************************************************/
+// .dkill conv - Dungeon Keeper Independent Level Layout converter for KeeperFX
+/******************************************************************************/
+/** @file dkillconv_version.h
+ *     Project name, version, copyrights and global definitions.
+ * @par Purpose:
+ *     Header file for global names and defines used by resource compiler.
+ * @par Comment:
+ *     Can only contain commands which resource compiler can understand.
+ * @author   Tomasz Lis
+ * @date     15 Jan 2015 - 28 Jan 2015
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef DKILLCONV_VERSION_H
+#define DKILLCONV_VERSION_H
+
+/* Version definitions */
+#define VER_MAJOR         1
+#define VER_MINOR         0
+#define VER_RELEASE       0
+#define VER_BUILD         0
+#define VER_STRING        "1.0.0.0"
+/* Program name, copyrights and file names */
+#define PROGRAM_NAME      "dkillconv"
+#define PROGRAM_FULL_NAME ".dkill conv - Dungeon Keeper Independent Level Layout  converter"
+#define PROGRAM_AUTHORS   "Tomasz Lis"
+#define COMPANY_NAME      "Community based"
+#define INTERNAL_NAME     "dkillconv"
+#define LEGAL_COPYRIGHT   "GNU General Public License"
+#define LEGAL_TRADEMARKS  ""
+#define FILE_VERSION VER_STRING
+#define FILE_DESCRIPTION PROGRAM_NAME
+#define ORIGINAL_FILENAME INTERNAL_NAME".exe"
+#define PRODUCT_NAME PROGRAM_FULL_NAME
+#define PRODUCT_VERSION    VER_STRING
+#define DEFAULT_LOG_FILENAME INTERNAL_NAME".log"
+
+#endif /*DKILLCONV_VERSION_H*/
+/******************************************************************************/
